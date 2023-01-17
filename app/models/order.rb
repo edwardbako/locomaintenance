df1bdcd91d60a3bdefe8a1b3d6b025b7750bc777
Edward Bako <edward.bako@gmail.com>
2023-01-17 20:59:53 +0700

    Add status for assigned orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,16 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :contact_person, reject_if: :all_blank
 
   enum status: [:newborn, :in_progress, :completed, :canceled]
+
+  before_save :set_in_progress, if: [:newborn?, :assigned?]
+
+  private
+
+  def set_in_progress
+    self.status = :in_progress
+  end
+
+  def assigned?
+    executor.present?
+  end
 end
